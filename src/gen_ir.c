b944a9e58600449d6a1021620dfc1c296e70e3f4
luyiming <luyimingchn@gmail.com>
2018-01-12 20:28:22 +0800

    fix Heisenbug
    uninitialized memory when inserting 'read' function symbol cause typeCheck() behave randomly

--- src/gen_ir.c
+++ src/gen_ir.c
@@ -43,17 +43,28 @@ int main(int argc, char **argv) {
     initSymbolTabel();
 
     Symbol read_func = (Symbol)malloc(sizeof(struct SymbolList_));
+    memset(read_func, 0, sizeof(struct SymbolList_));
     strcpy(read_func->name, "read");
     read_func->kind = FUNC_DEF;
+    read_func->tail = NULL;
     read_func->u.func = (Func)malloc(sizeof(struct Func_));
+    memset(read_func->u.func, 0, sizeof(struct Func_));
     read_func->u.func->definition = (ASTNode*)malloc(sizeof(ASTNode)); // to make checkUndefinedFunc() happy
+    memset(read_func->u.func->definition, 0, sizeof(ASTNode));
+    read_func->u.func->retType = (Type)malloc(sizeof(struct Type_)); // use in semantic.c - typeEqual(Type t1, Type t2)
+    memset(read_func->u.func->retType, 0, sizeof(struct Type_));
+    read_func->u.func->retType->kind = BASIC;
+    read_func->u.func->retType->u.basic = TYPE_INT;
     insertSymbol(read_func);
 
     Symbol write_func = (Symbol)malloc(sizeof(struct SymbolList_));
+    memset(write_func, 0, sizeof(struct SymbolList_));
     strcpy(write_func->name, "write");
     write_func->kind = FUNC_DEF;
     write_func->u.func = (Func)malloc(sizeof(struct Func_));
+    memset(write_func->u.func, 0, sizeof(struct Func_));
     write_func->u.func->definition = (ASTNode*)malloc(sizeof(ASTNode)); // to make checkUndefinedFunc() happy
+    memset(write_func->u.func->definition, 0, sizeof(ASTNode));
     insertSymbol(write_func);
 
     semantic_parse(ASTroot);
